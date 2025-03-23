bd77b9e2af1b73189bde99fa2595c1b32f968d56
Семён Черемисин <svcheremisin@edu.hse.ru>
2025-03-23 21:40:19 +0300

    Updated main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
+from fastapi import FastAPI
 from app.api.main import router as link_router
 from app.auth.auth import router as auth_router
-from fastapi import FastAPI
+import os
+from app.init_db import init_models
+import asyncio
 
 app = FastAPI(title="URL Shortener")
 
 app.include_router(link_router)
 app.include_router(auth_router)
+
+@app.on_event("startup")
+async def on_startup():
+    if os.getenv("INIT_DB_ON_STARTUP") == "true":
+        await init_models()
