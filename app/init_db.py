0e29e7e9b34514a023f0dece46646610ec6c8691
Семён Черемисин <svcheremisin@edu.hse.ru>
2025-03-23 21:47:21 +0300

    Remove useless code from init_db.py

--- app/init_db.py
+++ app/init_db.py
@@ -1,8 +1,5 @@
 from app.db import engine, Base
-import asyncio
 
 async def init_models():
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
-asyncio.run(init_models())
+        await conn.run_sync(Base.metadata.create_all) 
